33ad975e4037d90e193351ef13e6522853f81263
cosmoglint <ramprakashsmail@gmail.com>
2021-11-21 18:47:16 +0530

    ADD: configs for urls and file storage locations

--- config.js
+++ config.js
@@ -8,7 +8,9 @@ module.exports = {
   port: parseInt(process.env.MINIO_PORT),
   useSSL: (process.env.USE_SSL.toLowerCase() === 'true'),
   accessKey: process.env.ACCESS_KEY,
-  secretKey: process.env.SECRET_KEY
+  secretKey: process.env.SECRET_KEY,
+  bucketName: process.env.BUCKET_NAME,
+  imageLocationLocal: process.env.IMAGE_LOCATION_LOCAL
 }
 
 
