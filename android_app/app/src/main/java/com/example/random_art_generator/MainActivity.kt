3989e8cfb4e82e6b78d0fb494dbe5edec6ed2a2c
cosmoglint <ramprakashsmail@gmail.com>
2021-12-31 17:40:39 +0530

    ADD:  image view with loaded image

--- android_app/app/src/main/java/com/example/random_art_generator/MainActivity.kt
+++ android_app/app/src/main/java/com/example/random_art_generator/MainActivity.kt
@@ -6,24 +6,25 @@ import android.widget.Button
 import android.widget.TextView
 import java.util.*
 import android.widget.Toast
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.hello_android)
-        val rollButton: Button = findViewById(R.id.roll_button)
-        rollButton.setOnClickListener {
-            rollDice()
-
-        }
+         val myCanvas: ImageView = findViewById(R.id.my_canvas)
+//      val rollButton: Button = findViewById(R.id.my_canvas)
+//        rollButton.setOnClickListener {
+//            rollDice()
+//        }
 
     }
 
-    private fun rollDice() {
-        val rollValue: TextView = findViewById(R.id.roll_value)
+    // private fun rollDice() {
+//        val rollValue: TextView = findViewById(R.id.roll_value)
 //        val randval: Int = Random().nextInt(6) + 1
-        val curval: String = rollValue.text.toString()
-        val randval: Int = Integer.parseInt(curval) + 1
-        rollValue.text = randval.toString()
-    }
+//        val curval: String = rollValue.text.toString()
+//        val randval: Int = Integer.parseInt(curval) + 1
+//        rollValue.text = randval.toString()
+    //}
 }
