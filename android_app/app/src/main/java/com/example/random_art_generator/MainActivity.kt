492be28835d00f54f26a6c43ccaf812915421c07
cosmoglint <ramprakashsmail@gmail.com>
2021-12-31 18:29:02 +0530

    ADD:  click listener for canvas

--- android_app/app/src/main/java/com/example/random_art_generator/MainActivity.kt
+++ android_app/app/src/main/java/com/example/random_art_generator/MainActivity.kt
@@ -12,19 +12,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.hello_android)
-         val myCanvas: ImageView = findViewById(R.id.my_canvas)
-//      val rollButton: Button = findViewById(R.id.my_canvas)
-//        rollButton.setOnClickListener {
-//            rollDice()
-//        }
-
+        val myCanvas: ImageView = findViewById(R.id.my_canvas)
+        myCanvas.setOnClickListener {
+            canvasClicked()
+        }
     }
 
-    // private fun rollDice() {
-//        val rollValue: TextView = findViewById(R.id.roll_value)
-//        val randval: Int = Random().nextInt(6) + 1
-//        val curval: String = rollValue.text.toString()
-//        val randval: Int = Integer.parseInt(curval) + 1
-//        rollValue.text = randval.toString()
-    //}
+    private fun canvasClicked() {
+        Toast.makeText(this, "canvas clicked", Toast.LENGTH_SHORT).show()
+    }
 }
