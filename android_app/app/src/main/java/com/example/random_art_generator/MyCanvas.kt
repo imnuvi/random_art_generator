0ad18558c5e00c1946a77be0180f2f78c726978d
cosmoglint <ramprakashsmail@gmail.com>
2022-01-01 14:21:18 +0530

    ADD:  separate class for the segment object

--- android_app/app/src/main/java/com/example/random_art_generator/MyCanvas.kt
+++ android_app/app/src/main/java/com/example/random_art_generator/MyCanvas.kt
@@ -6,39 +6,79 @@ import android.content.Context
 import android.graphics.Paint
 import android.util.AttributeSet
 import androidx.core.content.res.ResourcesCompat
+import kotlin.math.min
 
-
-class MyCanvasView @JvmOverloads constructor(context: Context, attrs: AttributeSet?= null, defStyleAttr: Int=0) : ImageView(context, attrs, defStyleAttr){
-    private lateinit var my_canvas : Canvas
+class MyCanvasView @JvmOverloads constructor(context: Context, attrs: AttributeSet?= null, defStyleAttr: Int=0) : ImageView(context, attrs, defStyleAttr) {
+    private lateinit var my_canvas: Canvas
 
     private var drawcolor = ResourcesCompat.getColor(resources, R.color.white, null)
     private var my_paint = Paint().apply {
         color = drawcolor
         style = Paint.Style.FILL
     }
+    private var ww: Int = 0
+    private var wh: Int = 0
+    private var max_possible_width: Int = min(ww, wh)
+    private var minwidth: Int = 50
+    private var segment_size: Int = 0
+    private var segments: Int = 20
+    private var segmentList: List<Segment> = listOf()
+
+    private var minheight: Int = 50
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         // todo rerender the data with the updated sizes
         super.onSizeChanged(w, h, oldw, oldh)
+        this.ww = w
+        this.wh = h
+        segmentList = createSegments()
+        this.calculateSegmentSize()
     }
 
     // this function overload defines how the sketch is rendered on the screen. we will eventually save the image and set it as background
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
         renderRandom()
-        val content : Pair<Int, Int> = this.helper(4)
-        val xpos : Int = content.first
-        val ypos : Int = content.second
-        canvas?.drawText(xpos.toString().plus(ypos.toString()), 200f, 100f, my_paint)
+        val content: Pair<Int, Int> = this.helper(4)
+        val xpos: Int = content.first
+        val ypos: Int = content.second
+        this.renderCircle(canvas)
+        //canvas?.drawText(xpos.toString().plus(ypos.toString()), 200f, 100f, my_paint)
 //        canvas?.drawLine(10f,10f,120f,120f, my_paint)
+    }
+
+    private fun createSegments() {
+
+    }
+}
+
+class Segment constructor(xpos: Int, ypos: Int, width: Int, height: Int){
+
+    private fun calculateSegmentSize() {
+        if (max_possible_width/segments < minwidth)
+        {
+            segment_size = minwidth
+        }
+        else
+        {
+            segment_size = minwidth
+        }
+    }
+
+    private fun show(){
+       // split the screen into panes
+
     }
 
     private fun renderRandom(){
         // todo create a random function for rendering each style and colors
     }
 
-    // the helper functions should just return values that the main function will render
-    private fun renderCircle() {
+    // the helper functions will take the canvas element and draw the shape in it
+    private fun renderCircle(canvas: Canvas?) {
+        canvas?.drawCircle(100f, 200f, 100f, my_paint)
+    }
+    private fun renderTriangle(canvas: Canvas?) {
 
     }
     private fun helper(location_number: Int): Pair<Int, Int> {
